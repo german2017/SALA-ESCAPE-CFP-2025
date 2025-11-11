1dff3c95313dd5f468b0175f333e7201f5eada6a
KevinMoncayo <nano2916@gmail.com>
2025-11-11 20:33:24 -0300

    Inicio de intervención turno noche

--- ScapeRoomCFP/obj/Debug/net8.0-windows/ScapeRoomCFP.AssemblyInfo.cs
+++ ScapeRoomCFP/obj/Debug/net8.0-windows/ScapeRoomCFP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScapeRoomCFP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342ace0b3c7414fef903ccddf5068cba5eea18a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScapeRoomCFP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScapeRoomCFP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
